8813e09391d2b732a44d534abeb0c83d2c6dba05
MartinGLouw <ST10208767@vgconnect.edu.za>
2023-05-25 16:26:54 +0200

    adjustments

--- GADE_PART1/Assets/Invincibile.cs
+++ GADE_PART1/Assets/Invincibile.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Invincibile : MonoBehaviour
 {
+    void Start()
+    {
+        gameObject.tag = "Player";
+    }
     private IEnumerator ChangeTagBack(GameObject player)
     {
         yield return new WaitForSeconds(20);
